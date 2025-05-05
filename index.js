d83affd46c4aae82045950c687620a62386e6346
Andrew Buhler <terminalvelocity13@gmail.com>
2025-05-04 21:29:15 -0400

    Update 3 - Incorporated jQuery into project. - The function screen shows 1 when pressed and clear removes content from the screen.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+window.onload = function () {
+  var screen = "";
+  $("button").on("click", function () {
+    if ($(this).attr("id") === "clear") {
+      updateScreen(screen, "");
+    } else if ($(this).attr("id") === "one") {
+      updateScreen(screen, 1);
+    } // else if
+  });
 
+  function updateScreen(screen, value) {
+    screen += value;
+    $("#function-screen").text(screen);
+  } // updateScreen
+};
