cecfa02aaf4c854f0d33bc87e7654290b3cd0c65
Andrew Buhler <terminalvelocity13@gmail.com>
2025-05-07 09:35:16 -0400

    Update 5 - Numbers 0-9 are now functional. - Clear button is now functional. - Top screen properly shows numbers being entered.

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-window.onload = function () {
-  var screen = "";
-  $("button").on("click", function () {
-    if ($(this).attr("id") === "clear") {
-      updateScreen(screen, "");
-    } else if ($(this).attr("id") === "one") {
-      updateScreen(screen, 1);
-    } // else if
-  });
+var screenDisplay = "";
 
-  function updateScreen(screen, value) {
-    screen += value;
-    $("#function-screen").text(screen);
-  } // updateScreen
-};
+$("button").on("click", function () {
+  if ($(this).attr("id") === "clear") {
+    // clear button
+    let newScreenDisplay = "";
+    screenDisplay = newScreenDisplay;
+    $("#function-screen").text(newScreenDisplay);
+  } else if ($(this).attr("id") === "one") {
+    // one button
+    let newScreenDisplay = "1";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "two") {
+    // two button
+    let newScreenDisplay = "2";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "three") {
+    // three button
+    let newScreenDisplay = "3";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "four") {
+    // four button
+    let newScreenDisplay = "4";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "five") {
+    // five button
+    let newScreenDisplay = "5";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "six") {
+    // six button
+    let newScreenDisplay = "6";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "seven") {
+    // seven button
+    let newScreenDisplay = "7";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "eight") {
+    // eight button
+    let newScreenDisplay = "8";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "nine") {
+    // nine button
+    let newScreenDisplay = "9";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } else if ($(this).attr("id") === "zero") {
+    // zero button
+    let newScreenDisplay = "0";
+    screenDisplay += newScreenDisplay;
+    $("#function-screen").text(screenDisplay);
+  } // else if
+});
